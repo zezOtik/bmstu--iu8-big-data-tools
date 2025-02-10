beb9ccbb3d400c463222962df8e43797541de7c3
60130379 <Mikhail.Zotov@leroymerlin.ru>
2025-02-10 15:43:33 +0300

    add sql examples

--- src/pg/table/select.sql
+++ src/pg/table/select.sql
@@ -4,5 +4,5 @@ select * from users;
 -- получить максимальный регион
 select max(region) from users;
 
--- получить минимальный регион
+-- получить минимальный регион-
 select min(region) from users;
