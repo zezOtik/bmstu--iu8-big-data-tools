77982e64b0ecc5cbad231c72e1abfc26741b6c52
Michael Zotov <wzomzot@gmail.com>
2025-02-18 18:04:47 +0300

    Update create_table.sql

--- src/pg/table/create_table.sql
+++ src/pg/table/create_table.sql
@@ -9,3 +9,15 @@ CREATE TABLE users
     created_dttm timestamp,
     updated_dttm timestamp
 );
+
+
+
+
+CREATE TABLE src_store.order_details (
+order_details_id bigint PRIMARY key,
+sku_id bigint,
+order_id bigint REFERENCES src_store.order_head (order_id),
+created_date timestamp not null,
+updated_date timestamp not null,
+deleted_date timestamp null
+);
