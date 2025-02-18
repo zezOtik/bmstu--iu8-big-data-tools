c7d6a82e900c2b1672de864de5034833d9ebf0b6
Michael Zotov <wzomzot@gmail.com>
2025-02-18 18:05:12 +0300

    Update alter.sql

--- src/pg/table/alter.sql
+++ src/pg/table/alter.sql
@@ -3,3 +3,9 @@ alter table users add column region smallint;
 
 -- удаление колонки
 alter table users drop column region;
+
+
+alter table order_head 
+add constraint fk_users_user_id
+foreign key (user_id)
+references users (id);
