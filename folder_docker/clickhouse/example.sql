7a0cbd263a9c5f5a4479ddd3bc2edbf6e806197f
60130379 <Mikhail.Zotov@lemanapro.ru>
2026-04-06 15:57:36 +0300

    add clockhouse + superset

---
+++ folder_docker/clickhouse/example.sql
@@ -0,0 +1,167 @@
+
+-- Ручная схема данных
+CREATE TABLE default.taxi_info_2
+(
+    VendorID Int32,
+    tpep_pickup_datetime DateTime64(6),
+    tpep_dropoff_datetime DateTime64(6),
+    passenger_count Nullable(Int64),
+    trip_distance Nullable(Float64),
+    RatecodeID Nullable(Int64),
+    store_and_fwd_flag Nullable(String),
+    PULocationID Int32,
+    DOLocationID Int32,
+    payment_type Nullable(Int64),
+    fare_amount Nullable(Float64),
+    extra Nullable(Float64),
+    mta_tax Nullable(Float64),
+    tip_amount Nullable(Float64),
+    tolls_amount Nullable(Float64),
+    improvement_surcharge Nullable(Float64),
+    total_amount Nullable(Float64),
+    congestion_surcharge Nullable(Float64),
+    Airport_fee Nullable(Float64)
+)
+
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(tpep_pickup_datetime)
+ORDER BY (tpep_pickup_datetime, tpep_dropoff_datetime, PULocationID, DOLocationID)
+SETTINGS index_granularity = 8192;
+
+
+INSERT INTO default.taxi_info_2
+select * from default.taxi_info;
+where 1 = 1;
+
+SELECT
+    name,                  -- Имя части (папки на диске)
+    partition,             -- Партиция
+    disk,                  -- Диск
+    path,                  -- Полный путь к папке части
+    rows,                  -- Количество строк
+    bytes_on_disk,         -- Размер на диске (сжато)
+    data_compressed_bytes, -- Размер сжатых данных
+    data_uncompressed_bytes, -- Размер несжатых данных
+    marks_bytes,           -- Размер файлов меток (.mrk)
+    modification_time,     -- Время последнего изменения
+    active,                -- 1 = часть активна (участвует в SELECT), 0 = помечена на слияние/удаление
+    level,                 -- Уровень иерархии слияния (0 = свежая, >0 = результат слияния)
+    compression_codec      -- Кодек сжатия
+FROM system.parts
+WHERE table = 'my_table'
+  AND active = 1
+ORDER BY modification_time DESC;
+
+
+-- статистика по таблице
+SELECT
+    table,
+    count() as parts_count,           -- Сколько частей
+    sum(rows) as total_rows,          -- Всего строк
+    formatReadableSize(sum(bytes_on_disk)) as total_size, -- Размер на диске
+    formatReadableSize(sum(data_uncompressed_bytes)) as uncompressed_size,
+    sum(data_uncompressed_bytes) / sum(bytes_on_disk) as compression_ratio, -- Коэфф. сжатия
+    avg(rows) as avg_rows_per_part    -- В среднем строк на часть
+FROM system.parts
+WHERE table = 'taxi_info' AND active = 1
+GROUP BY table;
+
+-- /var/lib/clickhouse/data/default/my_table/all_1_1_0 путь до part
+
+select * from system.parts;
+
+INSERT INTO `default`.taxi_info
+(VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, trip_distance, RatecodeID, store_and_fwd_flag, PULocationID, DOLocationID, payment_type, fare_amount, extra, mta_tax, tip_amount, tolls_amount, improvement_surcharge, total_amount, congestion_surcharge, Airport_fee, cbd_congestion_fee)
+VALUES(0, '', '', 0, 0, 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+
+
+-- Определим схему данных автоматом и отдадим движку на откуп
+CREATE TABLE taxi_info AS
+SELECT *
+FROM file('/var/lib/clickhouse/user_files/import/yellow_tripdata_2026-01.parquet', Parquet)
+LIMIT 0;
+
+select * from taxi_info;
+
+
+-- Затем вставим данные:
+INSERT INTO taxi_info
+SELECT *
+FROM file('/var/lib/clickhouse/user_files/import/yellow_tripdata_2025-12.parquet', Parquet);
+
+CREATE TABLE taxi_sum
+(
+    VendorID Int32,
+    trip_distance Float64
+)
+ENGINE = SummingMergeTree()
+ORDER BY VendorID;
+
+insert into taxi_sum
+select
+VendorID,
+trip_distance
+from taxi_info;
+
+select * from taxi_sum;
+
+select * from taxi_info;
+
+create table covid_stats(
+date DATE,
+country String,
+confrimed Int64,
+recovered Int64,
+death Int64
+)
+ENGINE = MergeTree
+Order by (date, country);
+
+INSERT INTO default.covid_stats
+SELECT * FROM file(
+  '/var/lib/clickhouse/user_files/import/countries-aggregated-iso.csv',
+  'CSV',
+  'date Date, country String, confirmed Int64, recovered Int64, deaths Int64'
+);
+
+;
+
+truncate covid_stats;
+select distinct country from default.covid_stats;
+
+
+explain (
+        SELECT sum(total_amount), avg(total_amount), count(1)
+        FROM my_table
+)
+
+-- список частей
+SELECT _part
+FROM taxi_info
+GROUP BY _part
+ORDER BY _part ASC;
+
+-- список частей и уровень слияний
+-- 0 не учавствовала в слияние + 1 количество слияний
+SELECT
+    name,
+    level,
+    rows
+FROM system.parts
+WHERE (database = 'default') AND (`table` = 'taxi_info') AND active
+ORDER BY name ASC;
+
+-- Мутации
+ALTER TABLE default.taxi_info UPDATE passenger_count = passenger_count + 2 WHERE TRUE;
+
+
+--Посмотреть мутацию
+select * from system.mutations;
+
+-- Посмотреть движок таблицы
+SELECT engine
+FROM system.tables
+WHERE name = 'my_table';
+
+
